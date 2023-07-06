c9c271052f629dc3459231654caaa005745799ac
oktober13 <Robertxp129@gmail.com>
2023-07-06 16:13:03 +0500

    Update code :stethoscope:

--- project/orders/models.py
+++ project/orders/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
+from products.models import Product
 
-# Create your models here.
+
+class SalesOrder(models.Model):
+    amount = models.IntegerField()
+    description = models.CharField(max_length=255)
+    quantity = models.IntegerField(default=1)
+    title = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    products = models.ManyToManyField(Product)
