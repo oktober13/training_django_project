1a136ac1b729b3c8739e9711a3ac13ede0d7cb8c
oktober13 <Robertxp129@gmail.com>
2023-07-07 15:04:15 +0500

    Add templates :stethoscope:

--- project/orders/views.py
+++ project/orders/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def orders_page(request):
+    return render(request, template_name='index.html')
--- project/project/urls.py
+++ project/project/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from orders.views import orders_page
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', orders_page)
 ]
