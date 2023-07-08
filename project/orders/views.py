1e7175e82323b52d9be38519a83b7c26baa80b1f
oktober13 <Robertxp129@gmail.com>
2023-07-08 14:40:02 +0500

    Add API Djangorestframework :sparkles:

--- project/orders/views.py
+++ project/orders/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
 from orders.models import SalesOrder
+from orders.serializers import OrderSerializer
 
 
 # Create your views here.
 def orders_page(request):
     return render(request, 'index.html', {'orders': SalesOrder.objects.all()})
+
+
+class OrderView(ModelViewSet):
+    queryset = SalesOrder.objects.all()
+    serializer_class = OrderSerializer
--- project/project/urls.py
+++ project/project/urls.py
@@ -16,10 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework.routers import SimpleRouter
 
-from orders.views import orders_page
+from orders.views import orders_page, OrderView
+
+router = SimpleRouter()
+
+router.register('api/orders', OrderView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', orders_page)
 ]
+
+urlpatterns += router.urls
